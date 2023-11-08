041d34eb10dc7a954ef4b0b539911d2f45d2475b
Kateryna <korickaakaterina@gmail.com>
2023-11-08 09:48:59 +0000

    update files

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Logo } from "./components/Logo/Logo";
 import { NotFound } from "./pages/NotFound";
 import { ItemsNews } from "./pages/ItemsNews";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import styles from './App.css';
 
 
 function App() {
@@ -11,9 +12,9 @@ function App() {
       <BrowserRouter>
         <Logo />
         <Routes>
-          <Route path="*" element={<NotFound />} />
-          <Route path="/" element={<Home />} />
-          <Route path=":id" element={<ItemsNews />} />
+          <Route path="*" element={<NotFound />}></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path=":id" element={<ItemsNews />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
